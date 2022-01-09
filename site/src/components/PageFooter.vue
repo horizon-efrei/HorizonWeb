476c396ff2efaf257b1cba2563b713191cd16214
nicvlt <nicolas75d@gmail.com>
2022-01-09 19:02:03 +0100

    Footer

--- site/src/components/PageFooter.vue
+++ site/src/components/PageFooter.vue
@@ -1,40 +1,88 @@
 <template>
     <footer>
         <div class="px-6 pt-6 pb-2 border-t-2 border-footer">
-            <div class="flex flex-wrap justify-center">
-                <div class="w-full md:w-1/4 text-center text-0">
-                    <h5 class="uppercase md:mb-3 font-bold">
+            <div class="flex justify-center flex-wrap md:flex-nowrap md:gap-3/24">
+                <div class="text-0 hidden lg:block">
+                    <ul class="mb-4">
+                        <li class="mt-2 flex items-center gap-2">
+                            <i class="ri-newspaper-line" />
+                            <a
+                                href="#"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >  Actualité</a>
+                        </li>
+                        <li class="mt-2 items-center gap-2">
+                            <i class="ri-book-line" />
+                            <a
+                                href="#"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >  Livre d'or</a>
+                        </li>
+                        <li class="mt-2 items-center gap-2">
+                            <i class="ri-bookmark-line" />
+                            <a
+                                href="#"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >  Revue de presse</a>
+                        </li>
+                        <li class="mt-2 items-center gap-2">
+                            <i class="ri-shopping-bag-line" />
+                            <a
+                                href="#"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >  Boutique</a>
+                        </li>
+                        <li class="mt-2 items-center gap-2">
+                            <i class="ri-mail-line" />
+                            <a
+                                href="#"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >  Nous contacter</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="text-0 w-full md:w-fit">
+                    <h5 class="text-blue-300 uppercase md:mb-3 font-bold">
                         Liens
                     </h5>
                     <ul class="mb-4">
                         <li class="mt-2">
                             <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
                             >FAQ</a>
                         </li>
                         <li class="mt-2">
                             <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
                             >Aide</a>
                         </li>
                         <li class="mt-2">
                             <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
                             >Support</a>
                         </li>
                     </ul>
                 </div>
-                <div class="w-full md:w-1/4 text-center text-0">
-                    <h5 class="uppercase mt-2 md:mt-0 md:mb-3 font-bold">
+                <div class="text-0 w-full md:w-fit">
+                    <h5 class="text-blue-300 uppercase mt-2 md:mt-0 md:mb-3 font-bold">
                         Légalité
                     </h5>
                     <ul class="mb-4">
                         <li class="mt-2">
                             <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
                             >CGU</a>
                         </li>
@@ -46,47 +94,59 @@
                         </li>
                     </ul>
                 </div>
-                <div class="w-full md:w-1/4 text-center text-0">
-                    <h5 class="uppercase mt-2 md:mt-0 md:mb-3 font-bold">
-                        Réseaux
-                    </h5>
-                    <ul class="mb-4">
-                        <li class="mt-2">
-                            <a
-                                href="https://www.instagram.com/horizon.efrei/"
-                                class="hover:underline text-2 hover:text-orange-500"
-                            >Instagram</a>
-                        </li>
-                        <li class="mt-2">
-                            <a
-                                href="https://discord.gg/q22FW5xhvP"
-                                class="hover:underline text-2 hover:text-orange-500"
-                            >Discord</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="w-full md:w-1/4 text-center text-0">
-                    <h5 class="uppercase mt-2 md:mt-0 md:mb-3 font-bold">
+                <div class="text-0 w-full md:w-fit">
+                    <h5 class="text-blue-300 uppercase mt-2 md:mt-0 md:mb-3 font-bold">
                         Horizon
                     </h5>
                     <ul class="mb-4">
                         <li class="mt-2">
                             <a
+                                href="https://discord.gg/bHah8faFaj"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >Web</a>
+                        </li>
+                        <li class="mt-2">
+                            <a
+                                href="  https://discord.gg/G7fWxQZXqF"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >Mentorat</a>
+                        </li>
+                        <li class="mt-2">
+                            <a
+                                href="https://discord.gg/kpcMs8BT8u"
+                                target="_blank"
+                                class="hover:underline text-2 hover:text-orange-500"
+                            >Ef'Réussite</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="text-0 w-full md:w-fit">
+                    <h5 class="text-blue-300 uppercase mt-2 md:mt-0 md:mb-3 font-bold">
+                        A propos
+                    </h5>
+                    <ul class="mb-4">
+                        <li class="mt-2">
+                            <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
                             >Blog</a>
                         </li>
                         <li class="mt-2">
                             <a
-                                href="#"
+                                href="https://www.instagram.com/horizon.efrei/"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
-                            >Notre équipe</a>
+                            >Instagram</a>
                         </li>
                         <li class="mt-2">
                             <a
                                 href="#"
+                                target="_blank"
                                 class="hover:underline text-2 hover:text-orange-500"
-                            >Contact</a>
+                            >Notre équipe</a>
                         </li>
                     </ul>
                 </div>
