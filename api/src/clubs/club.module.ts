b1ac51f19f92536c4b10a3833873e9cb71d435b2
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-01-11 10:29:40 +0100

    correct club module

--- api/src/clubs/club.module.ts
+++ api/src/clubs/club.module.ts
@@ -4,6 +4,7 @@ import { Post } from '../posts/entities/post.entity';
 import { CaslAbilityFactory } from '../shared/modules/casl/casl-ability.factory';
 import { UsersModule } from '../users/users.module';
 import { ClubMember } from './club-member.entity';
+import { ClubSearchService } from './club-search.service';
 import { ClubsController } from './club.controller';
 import { Club } from './club.entity';
 import { ClubsService } from './club.service';
@@ -14,7 +15,7 @@ import { ClubsService } from './club.service';
     UsersModule,
   ],
   controllers: [ClubsController],
-  providers: [CaslAbilityFactory, ClubsService],
+  providers: [CaslAbilityFactory, ClubsService, ClubSearchService],
   exports: [ClubsService],
 })
 export class ClubsModule {}
