457d4bd38f8acdae5927bb1c59b4a7d371f7c777
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-01-17 17:27:08 +0100

    start of shadow inset on avatar(not workng yet) and some fixs

--- site/src/components/AvatarImage.vue
+++ site/src/components/AvatarImage.vue
@@ -1,17 +1,19 @@
 <template>
-    <img
-        :src="(avatar==='' || avatar=== null ||avatar === undefined) ? default_avatar :avatar"
-        :alt="alt"
-        class="rounded-full bg-1 shadow-inner"
-        :class="`w-${size} h-${size}`"
-    >
+    <div class="shadow rounded-full bg-1">
+        <img
+            :src="(src==='' || src=== null ||src === undefined) ? default_avatar :src"
+            :alt="alt"
+            class="rounded-full "
+            :class="`w-${size} h-${size}`"
+        >
+    </div>
 </template>
 <script>
 import default_avatar from '@/assets/img/default_avatars/user.png'
 
 export default {
     props: {
-        avatar: {
+        src: {
             type: String,
             default: () => ""
         },
@@ -33,5 +35,8 @@ export default {
 }
 </script>
 <style>
+    .shadow{
+        box-shadow:inset 0 2px 4px 0 hsla(0,0%,0%,.2);
 
+    }
 </style>
