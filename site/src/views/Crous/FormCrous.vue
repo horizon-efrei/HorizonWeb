b813682964ec1a375265ba245c700258bed3b859
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-01-27 16:19:49 +0100

    Adding some securities on Crous Form

--- site/src/views/Crous/FormCrous.vue
+++ site/src/views/Crous/FormCrous.vue
@@ -43,9 +43,11 @@
                                 <p class=" text-sm text-blue-500">Ajouter une entree</p>
                             </button>
                         </div>
-                        <div v-for="(starter,idx) in menu.starters" :key="starter" class="mb-2">
+                        <div v-for="(starter,idx) in menu.starters" :key="starter" class="flex gap-2 mb-2">
                             <SelectInput v-model="menu.starters[idx]" :model-value="food.filter((a) => a.type === 1).indexOf(starter)" :max-content-width="true" :choices="food.filter((a) => a.type === 1).map(a => a.name)"></SelectInput>
+                            <button class="text-red-500" @click="removeStarter(idx)"><font-awesome-icon icon="times"></font-awesome-icon></button>
                         </div>
+                        <p v-if="starterInvalid" class="text-sm text-red-500">Veuillez remplir tous les champs</p>
                     </div>
                     <div class="w-1/3">
                         <div class="flex gap-2 items-center">
@@ -54,25 +56,38 @@
                                 <p class=" text-sm text-blue-500">Ajouter un plat</p>
                             </button>
                         </div>
-                        <div v-for="(dish,idx) in menu.dishes" :key="dish" class="mb-2">
+                        <div v-for="(dish,idx) in menu.dishes" :key="dish" class="flex gap-2 mb-2">
                             <SelectInput v-model="menu.dishes[idx]" :model-value="food.filter((a) => a.type === 2).indexOf(dish)" :max-content-width="true" :choices="food.filter((a) => a.type === 2).map(a => a.name)"></SelectInput>
+                            <button class="text-red-500" @click="removeDish(idx)"><font-awesome-icon icon="times"></font-awesome-icon></button>
                         </div>
+                        <p v-if="dishInvalid" class="text-sm text-red-500">Veuillez remplir tous les champs</p>
                     </div>
                     <div class="w-1/3">
                         <div class="flex gap-2 items-center">
                             <p class="text-md">Desserts</p>
                             <button class="" @click="addDessert()">
-                                <p class=" text-sm text-blue-500">Ajouter un dessert</p>
+                                <p class="text-sm text-blue-500">Ajouter un dessert</p>
                             </button>
                         </div>
-                        <div v-for="(dessert,idx) in menu.desserts" :key="idx" class="mb-2">
+                        <div v-for="(dessert,idx) in menu.desserts" :key="idx" class="flex gap-2 mb-2">
                             <SelectInput v-model="menu.desserts[idx]" :model-value="food.filter((a) => a.type === 3).indexOf(dessert)" :max-content-width="true" :choices="food.filter((a) => a.type === 3).map(a => a.name)"></SelectInput>
+                            <button class="text-red-500" @click="removeDessert(idx)"><font-awesome-icon icon="times"></font-awesome-icon></button>
                         </div>
+                        <p v-if="dessertInvalid" class="text-sm text-red-500">Veuillez remplir tous les champs</p>
                     </div>
                 </div>
-                <button class="mt-4 mb-8 button" @click="submitMenu()">
-                    <p>Enregistrer le Menu</p>
-                </button>
+                <div class="flex gap-2 mt-2 mb-8">
+                    <button class=" button" @click="submitMenu()">
+                        <p class="whitespace-nowrap">Enregistrer le Menu</p>
+                    </button>
+                    <AppAlert v-if="showMenuValidation" :type="menuFormStatus ? 'success' : 'error' ">
+                        <template #text>
+                            <div class="title">
+                                {{validateMenuMessage}}
+                            </div>
+                        </template>
+                    </AppAlert>
+                </div>
             </div>
             <div class="flex items-center">
                 <div class="text-lg font-bold">Un plat manque à la liste ?</div>
@@ -83,26 +98,41 @@
                     <div class="mb-2">
                         <div>Nom du plat</div>
                         <input v-model="foodForm.name" class="input">
+                        <p v-if="foodNameInvalid" class="text-sm text-red-500">Veuillez choisir un nom valide</p>
                     </div>
                     <div class="mb-2 ml-4">
                         <div >Type de Plat</div>
                         <SelectInput v-model="foodForm.type" :choices="['Entrée','Plat','Dessert']"></SelectInput>
+                        <p v-if="foodTypeInvalid" class="text-sm text-red-500">Veuillez choisir un type valide</p>
                     </div>
                 </div>
 
-                <button class="button" @click="submitFood()">
-                    <p>Enregistrer</p>
-                </button>
+                <div class="flex gap-2">
+                    <button class="button" @click="submitFood()">
+                        <p>Enregistrer</p>
+                    </button>
+                    <AppAlert v-if="showFoodValidation" :type="foodFormStatus ? 'success' : 'error' ">
+                        <template #text>
+                            <div class="title">
+                                {{validateFoodMessage}}
+                            </div>
+                        </template>
+                    </AppAlert>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import AppAlert from '@/components/App/AppAlert.vue';
 import SelectInput from '@/components/Input/SelectInput.vue';
 import { watch } from 'vue';
 
 export default {
-    components: { SelectInput },
+    components: {
+        SelectInput,
+        AppAlert,
+    },
     data() {
         return {
             menu: {
@@ -116,6 +146,17 @@ export default {
                 name: null,
                 type: null,
             },
+            foodTypeInvalid: false,
+            foodNameInvalid: false,
+            foodFormStatus: false,
+            validateFoodMessage: '',
+            showFoodValidation: false,
+            showMenuValidation: false,
+            menuFormStatus: false,
+            validateMenuMessage: '',
+            starterInvalid: false,
+            dishInvalid: false,
+            dessertInvalid: false,
         }
     },
     computed: {
@@ -157,23 +198,133 @@ export default {
         addStarter: function addStarter() {
             this.menu.starters.push(null)
         },
+        removeDessert: function removeDessert(idx) {
+            this.menu.desserts.splice(idx,1)
+        },
+        removeDish: function removeDish(idx) {
+            this.menu.dishes.splice(idx,1)
+        },
+        removeStarter: function removeStarter(idx) {
+            this.menu.starters.splice(idx,1)
+        },
         toggleFoodForm: function toggleFoodForm() {
             this.showFoodForm = !this.showFoodForm
+            this.showFoodValidation = false
         },
         submitMenu: function submitMenu() {
-            this.$store.dispatch('crous/postMenu', {
-                starters: this.menu.starters.map(a => a.foodId),
-                dishes: this.menu.dishes.map(a => a.foodId),
-                desserts: this.menu.desserts.map(a => a.foodId),
-                date: this.date,
-            })
+            this.showMenuValidation = false
+            if (this.isValidFormMenu()) {
+                this.$store.dispatch('crous/postMenu', {
+                    starters: this.menu.starters.map(a => a.foodId),
+                    dishes: this.menu.dishes.map(a => a.foodId),
+                    desserts: this.menu.desserts.map(a => a.foodId),
+                    date: this.date,
+                }).then(
+                    () => {
+                        this.validateMenuMessage = 'Création réussie'
+                        this.menuFormStatus = true
+                    },
+                    () => {
+                        this.validateMenuMessage = 'Création failed'
+                        this.menuFormStatus = false
+                    },
+                )
+            } else {
+                if (this.menu.starters.length === 0 || this.menu.dishes.length === 0 || this.menu.desserts.length === 0 ) {
+                    this.validateMenuMessage = 'Veuillez ajouter au moins un element'
+                } else {
+                    this.validateMenuMessage = 'Veuillez ne laisser aucun déroulant vide'
+                }
+                this.menuFormStatus = false
+            }
+            this.showMenuValidation = true
         },
         submitFood: function submitFood() {
-            this.$store.dispatch('crous/postFood', {
-                name: this.foodForm.name,
-                type: this.foodForm.type +1,
-            })
+            this.showFoodValidation = false
+            if ( this.isValidFormFood() ) {
+                this.$store.dispatch('crous/postFood', {
+                    name: this.foodForm.name,
+                    type: this.foodForm.type +1,
+                }).then(
+                    () => {
+                        this.validateFoodMessage = 'Création réussie'
+                        this.foodFormStatus = true
+                    },
+                    () => {
+                        this.validateFoodMessage = 'Création failed'
+                        this.foodFormStatus = false
+                    },
+                )
+            } else {
+                this.validateFoodMessage = 'Veuillez remplir tous les champs'
+                this.foodFormStatus = false
+            }
+            this.showFoodValidation = true
+            this.foodForm.name = null
+            this.foodForm.type = null
         },
+        isValidFormFood: function isValidFormFood() {
+            let a = true
+            if (this.foodForm.name === null || this.foodForm.name === '' || this.foodForm.name === undefined) {
+                this.foodNameInvalid = true
+                a = false
+            } else {
+                this.foodNameInvalid = false
+            }
+            if (this.foodForm.type === null || this.foodForm.type === undefined) {
+                this.foodTypeInvalid = true
+                a = false
+            } else {
+                this.foodTypeInvalid = false
+            }
+            return a
+        },
+        isValidFormMenu: function isValidFormMenu() {
+            let a = true
+            let b = true
+            for (const i of this.menu.starters) {
+                if (i === null) {
+                    b = false
+                    a = false
+                    this.starterInvalid = true
+                }
+            }
+            if (b) {
+                this.starterInvalid = false
+            } else {
+                b = true
+            }
+            for (const i of this.menu.dishes) {
+                if (i === null) {
+                    a = false
+                    b = false
+                    this.dishInvalid = true
+                }
+            }
+            if (b) {
+                this.dishInvalid = false
+            } else {
+                b = true
+            }
+            for (const i of this.menu.desserts) {
+                if (i === null) {
+                    a = false
+                    b = false
+                    this.dessertInvalid = true
+                }
+            }
+
+            if (b) {
+                this.dessertInvalid = false
+            } else {
+                b = true
+            }
+            if (this.menu.starters.length === 0 || this.menu.dishes.length === 0 || this.menu.desserts.length === 0 ) {
+                a = false
+            }
+            return a
+        },
+
     },
 }
 </script>
