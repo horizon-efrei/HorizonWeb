37406deb1f5e79b62a0c0fe448780801acd8d87f
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-03-08 11:27:08 +0100

    Début du store userProfile

--- site/src/store/profile.store.js
+++ site/src/store/profile.store.js
@@ -1,8 +1,6 @@
 import { defineStore } from 'pinia'
 import $axios from '../shared/config/axios.config'
 
-
-
 export const useProfilesStore = defineStore('profile', {
     state: () => ({
         user: null,
@@ -11,30 +9,24 @@ export const useProfilesStore = defineStore('profile', {
     }),
     actions: {
         changeUser(user) {
-            this.user = user;
-            return user;
+            this.user = user
+            return user
         },
         changeContacts(contacts) {
-            this.contacts = contacts;
-            return contacts;
+            this.contacts = contacts
+            return contacts
         },
         changeClubs(clubs) {
             this.clubs = clubs
         },
         async loadUser(userId) {
-            return await $axios
-                .get(`users/${userId}`)
-                .then((res) => this.changeUser(res.data))
+            return await $axios.get(`users/${userId}`).then((res) => this.changeUser(res.data))
         },
         async loadContacts(userId) {
-            return await $axios
-                .get(`contacts/user/${userId}`)
-                .then( (res) => this.changeContacts(res.data))
+            return await $axios.get(`contacts/user/${userId}`).then((res) => this.changeContacts(res.data))
         },
         async loadClubs(userId) {
-            return await $axios
-                .get(`club/memberships/${userId}`)
-                .then(res => this.changeClubs(res.data))
+            return await $axios.get(`club/memberships/${userId}`).then((res) => this.changeClubs(res.data))
         },
     },
 })
