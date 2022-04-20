c5ffba46c8d3fe253f809fffa980a264bb2f81e3
Blyzi <flash.spaces@gmail.com>
2022-04-20 14:31:07 +0200

    PWA

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,9 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [VitePWA({})],
+    plugins: [vue(), VitePWA({})],
     resolve: { alias: { '@': resolve(__dirname, 'src') } },
     server: { open: true },
 })
