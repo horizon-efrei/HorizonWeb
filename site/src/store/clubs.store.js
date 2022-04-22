04affd9c7d97fa9e207633c558d748f7e8590c3a
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-04-22 21:45:50 +0200

    starting of Clubs List page

---
+++ site/src/store/clubs.store.js
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia'
+import $axios from '../shared/config/axios.config'
+
+export const useClubsStore = defineStore('clubs', {
+    state: () => ({
+        clubs: [],
+        club: {},
+    }),
+    actions: {
+        replaceClubs(clubs) {
+            this.clubs = clubs
+            return clubs
+        },
+        replaceClub(club) {
+            this.club = club
+            return club
+        },
+        async getClubs() {
+            return await $axios.get('clubs').then((res) => this.replaceClubs(res.data))
+        },
+        async getClub(clubId) {
+            return await $axios.get(`clubs/${clubId}`).then((res) => this.replaceClub(res.data))
+        },
+        // async patchUser(props) {
+        //     return await $axios.patch('users', props).then((res) => this.replaceUser(res.data))
+        // },
+        // async postContact({ contactId, link, pseudo }) {
+        //     return await $axios
+        //         .post('/contacts/users', { contactId, link, pseudo })
+        //         .then((res) => this.replaceContacts(...this.contacts, res))
+        // },
+        // async patchContact(contact) {
+        //     return await $axios.patch()
+        // },
+    },
+})
